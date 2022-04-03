8856e18ab12e06f19d50613bd6fc96c1a8485f8d
Dylan <brousse.dylan@gmail.com>
2022-04-03 14:52:07 +0200

    [DEBUG]

--- Assets/Dylan/Scripts/Door.cs
+++ Assets/Dylan/Scripts/Door.cs
@@ -11,6 +11,7 @@ public class Door : MonoBehaviour
     [SerializeField] Vector2 directionWhenOpening;
     Vector2 initialPos;
     IEnumerator reset;
+    IEnumerator open;
 
 
     [Header("AUDIO")] [SerializeField] private AudioFade audioFade = null;
@@ -37,7 +38,10 @@ public class Door : MonoBehaviour
     {
         hiddenSwitchList.Remove(switchToRemove);
         if (hiddenSwitchList.Count == 0)
-            StartCoroutine(Opening());
+        {
+            open = Opening();
+            StartCoroutine(open);
+        }
     }
 
     public void OpenAction()
@@ -111,6 +115,7 @@ public class Door : MonoBehaviour
 
     IEnumerator ResetPosition()
     {
+        StopCoroutine(open);
         while (Vector2.Distance(transform.position, initialPos) != 0)
         {
             Vector2 pos = Vector2.MoveTowards(transform.position, initialPos, openSpeed * Time.deltaTime * 10);
