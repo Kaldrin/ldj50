404aeba027bc610b717131668aa498b568d8eec3
Bastien <bastien69580@gmail.com>
2022-04-04 12:17:59 +0200

    Face animations for speed & slow

--- Assets/Dylan/Scripts/PlayerSpeedModifiers.cs
+++ Assets/Dylan/Scripts/PlayerSpeedModifiers.cs
@@ -7,7 +7,9 @@ public class PlayerSpeedModifiers : MonoBehaviour
     Character chara;
     float startSpeed;
     [SerializeField] private ParticleSystem fastFX = null;
+    [SerializeField] private ParticleSystem fastFX2 = null;
     [SerializeField] private ParticleSystem slowFX = null;
+    [SerializeField] private ParticleSystem slowFX2 = null;
     
     
 
@@ -24,6 +26,13 @@ public class PlayerSpeedModifiers : MonoBehaviour
             // FX
             if (slowFX && !slowFX.isEmitting)
                slowFX.Play();
+            if (slowFX2 && !slowFX2.isEmitting)
+                slowFX2.Play();
+            
+            
+            // ANIMATION
+            chara.faceAnimator.SetBool("Slow", true);
+            
 
             chara.speed = startSpeed - startSpeed * 60 / 100;
         }
@@ -32,6 +41,12 @@ public class PlayerSpeedModifiers : MonoBehaviour
             // FX
             if (fastFX && !fastFX.isEmitting)
                 fastFX.Play();
+            if (fastFX2 && !fastFX2.isEmitting)
+                fastFX2.Play();
+            
+            
+            // ANIMATION
+            chara.faceAnimator.SetBool("Fast", true);
 
             chara.speed = startSpeed + startSpeed * 60 / 100;
         }
@@ -44,8 +59,17 @@ public class PlayerSpeedModifiers : MonoBehaviour
             // FX
             if (fastFX)
                 fastFX.Stop();
+            if (fastFX2)
+                fastFX2.Stop();
             if (slowFX)
                 slowFX.Stop();
+            if (slowFX2)
+                slowFX2.Stop();
+            
+            
+            // ANIMATION
+            chara.faceAnimator.SetBool("Fast", false);
+            chara.faceAnimator.SetBool("Slow", false);
             
             
             chara.speed = startSpeed;
