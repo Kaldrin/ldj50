a192389505118a6aca7be92da9cfdd36d424aec4
Kaldrin <bastien69580@gmail.com>
2022-07-23 16:43:11 +0200

    [UP] Made the end level trigger a prefab

--- Assets/BASTIEN/Scripts/SelectionPriority.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-
-// This component makes its owner the selection priority when clicking on it or its children
-[SelectionBase]
-public class SelectionPriority : MonoBehaviour
-{ }
--- Assets/Dylan/Scripts/EndLevelTrigger.cs
+++ Assets/Dylan/Scripts/EndLevelTrigger.cs
@@ -1,10 +1,19 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
+/// <summary>
+/// Detects when a player enters it and ends the corresponding level, switching to the next one. Must be a grand child of the level GO.
+/// </summary>
 public class EndLevelTrigger : MonoBehaviour
 {
+    [SerializeField] Color color = Color.gray;
     private void OnTriggerEnter2D(Collider2D other) => transform.parent.parent.GetComponent<Level>().ChangeLevel();
-    private void OnDrawGizmos() => Gizmos.DrawCube(transform.position, transform.localScale);
+    /// <summary>
+    /// Display of the element in the scene
+    /// </summary>
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = color;
+        Gizmos.DrawCube(transform.position, transform.localScale);
+    }
 }
